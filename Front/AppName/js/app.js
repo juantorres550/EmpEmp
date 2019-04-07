44edf3890f5cf822965853ef969bff8f8e3b416b
Miguel A. Palacio <45249613+migpalg@users.noreply.github.com>
2019-04-06 23:39:51 -0500

    Script principal

---
+++ Front/AppName/js/app.js
@@ -0,0 +1,13 @@
+/**
+* Here comes the application context.
+* We'll use AJAX and PHP to make the queries to the database, and read the 
+* returned JSON correctly.
+*/
+
+(function(){
+     'use strict';
+     document.addEventListener('DOMContentLoaded', function(){
+          // Here comes the app
+          console.log("Hello world!");
+     })
+})();
